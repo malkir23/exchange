3f3df3b5d50ef51d73d6b69acde4532ee45e1a66
malkir23 <mandragorand@gmail.com>
2024-12-29 13:14:25 +0100

    feat: enhance TWAPs chart and data fetching with improved error handling and duration display

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,7 +1,7 @@
 const TWAPsChartKey = 'TWAPs-chart';
 
 let dataExchange = {
-  TWAPsChartKey: [],
+  [TWAPsChartKey]: [],
 };
 let price = {};
 
@@ -12,157 +12,97 @@ async function buildChart(keyData) {
   const labels = TWAPsData.map((item) =>
     new Date(item.time).toLocaleDateString()
   );
-  console.log(dataExchange);
 
   const dataPoints = TWAPsData.map((item) => item.action.twap.s);
-  const sellDataPoints = TWAPsData.filter((item) => item.action.twap.b === false).map((item) => item.action.twap.s);
-  const buyDataPoints = TWAPsData.filter((item) => item.action.twap.b === true).map((item) => item.action.twap.s);
+  const sellDataPoints = TWAPsData.filter((item) => !item.action.twap.b).map((item) => item.action.twap.s);
+  const buyDataPoints = TWAPsData.filter((item) => item.action.twap.b).map((item) => item.action.twap.s);
 
   const ctx = document.getElementById(`${keyData}-data`).getContext('2d');
 
-  // Check if the chart already exists
   if (TWAPsChart) {
-    TWAPsChart.destroy(); // Destroy the existing chart
+    TWAPsChart.destroy();
   }
 
   TWAPsChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: labels,
+      labels,
       datasets: [
-        {
-          label: 'Daily Trades',
-          data: dataPoints,
-          borderColor: 'rgba(75, 192, 192, 1)',
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderWidth: 2,
-          tension: 0.3,
-        },
-        {
-          label: 'Sell',
-          data: sellDataPoints,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderWidth: 2,
-          tension: 0.3,
-        },
-        {
-          label: 'Buy',
-          data: buyDataPoints,
-          borderColor: 'rgb(5, 172, 69)',
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderWidth: 2,
-          tension: 0.3,
-        },
+        { label: 'Daily Trades', data: dataPoints, borderColor: 'rgba(75, 192, 192, 1)', backgroundColor: 'rgba(75, 192, 192, 0.2)', borderWidth: 2, tension: 0.3 },
+        { label: 'Sell', data: sellDataPoints, borderColor: 'rgba(255, 99, 132, 1)', backgroundColor: 'rgba(255, 99, 132, 0.2)', borderWidth: 2, tension: 0.3 },
+        { label: 'Buy', data: buyDataPoints, borderColor: 'rgb(5, 172, 69)', backgroundColor: 'rgba(54, 162, 235, 0.2)', borderWidth: 2, tension: 0.3 },
       ],
     },
     options: {
       responsive: true,
       plugins: {
-        legend: {
-          position: 'top',
-        },
-        tooltip: {
-          mode: 'index',
-          intersect: false,
-        },
+        legend: { position: 'top' },
+        tooltip: { mode: 'index', intersect: false },
       },
       scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Date',
-          },
-        },
-        y: {
-          title: {
-            display: true,
-            text: 'Trades',
-          },
-          beginAtZero: true,
-        },
+        x: { title: { display: true, text: 'Date' } },
+        y: { title: { display: true, text: 'Trades' }, beginAtZero: true },
       },
     },
   });
 }
 
-
 async function get_price(requestData) {
-  await $.ajax({
-    url: "https://api.hyperliquid.xyz/info",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(requestData),
-    success: function (response) {
-      price = response;
-    },
-    error: function (error) {
-      console.error("API Error:", error);
-    }
-  });
+  try {
+    const response = await $.ajax({
+      url: "https://api.hyperliquid.xyz/info",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(requestData),
+    });
+    price = response;
+  } catch (error) {
+    console.error("API Error:", error);
+  }
 }
 
 function addTotalData(totalData) {
-  console.log(totalData);
+  $('#total-sell').text(totalData.countSell || 0);
+  $('#total-buy').text(totalData.countBuy || 0);
+  $('#total-sell-value').text(`${totalData.volumeSell || 0} $`);
+  $('#total-buy-value').text(`${totalData.volumeBuy || 0} $`);
+}
 
-  $('#total-sell').text(totalData['countSell'] || 0);
-  $('#total-buy').text(totalData['countBuy'] || 0);
-  $('#total-sell-value').text(`${totalData['volumeSell']} $` || 0);
-  $('#total-buy-value').text(`${totalData['volumeBuy']} $` || 0);
+function formatTime(minutes) {
+  return minutes < 60 ? `${minutes} minutes` : `${Math.floor(minutes / 60)} hours ${minutes % 60} minutes`;
+}
+
+function calculateHours(timestamp) {
+  const date = new Date(timestamp - new Date().getTime());
+  return `${date.getUTCHours()} hours ${date.getUTCMinutes()} minutes`;
 }
 
 async function fetchTwaps() {
-  await get_price({ "tokenId": "0x0d01dc56dcaaca66ad901c959b4011ec", "type": "tokenDetails" });
-  let totalData = {};
+  await get_price({ tokenId: "0x0d01dc56dcaaca66ad901c959b4011ec", type: "tokenDetails" });
   $.ajax({
     url: '/twaps/get_twap_data',
     method: 'GET',
     success: function (data) {
-      let twapsTable = $('#TWAPs-table tbody');
-
+      const twapsTable = $('#TWAPs-table tbody');
       twapsTable.empty();
-      if (data && Array.isArray(data)) {
-        data.forEach(item => {
-          const isSell = item.action.twap.b === false;
+      if (Array.isArray(data)) {
+        const totalData = data.reduce((acc, item) => {
+          const isSell = !item.action.twap.b;
           const dollarValue = price.markPx * item.action.twap.s;
-          data['dollarValue'] = dollarValue;
-          function calculateHours(timestamp) {
-            // Конвертуємо мітку часу в об'єкт Date
-            const date = new Date(timestamp);
-
-            // Отримуємо години з урахуванням місцевого часу
-            const hours = date.getUTCHours(); // Використовуємо UTC для стабільності
-
-            // Повертаємо розраховані години
-            return hours;
-          }
-
-          const calculatedHours = calculateHours(item.time);
-          if(!item.ended) {
-
-            console.log("Calculated Hours:", item.time, calculatedHours);
-          }
-
           const row = `<tr>
-          <td class="${isSell ? 'sell-color' : 'buy-color'}">${isSell ? 'SELL' : 'BUY' || 'N/A'}</td>
-          <td>${item.action.twap.s || 'N/A'}</td>
-          <td>${dollarValue || 'N/A'} $</td>
-          <td>${item.user || 'N/A'}</td>
-          <td>${item.ended || calculatedHours}</td>
+            <td class="${isSell ? 'sell-color' : 'buy-color'}">${isSell ? 'SELL' : 'BUY'}</td>
+            <td>${item.action.twap.s || 'N/A'}</td>
+            <td>${dollarValue.toFixed(2)} $</td>
+            <td>${item.user || 'N/A'}</td>
+            <td>${formatTime(item.action.twap.m)}</td>
+            <td>${item.ended || calculateHours(item.time)}</td>
           </tr>`;
-
-          if (isSell) {
-            totalData['countSell'] = totalData['countSell'] ? totalData['countSell'] + 1 : 1;
-            totalData['volumeSell'] = totalData['volumeSell'] ? totalData['volumeSell'] + parseInt(item.action.twap.s) : parseInt(item.action.twap.s);
-          } else {
-            totalData['countBuy'] = totalData['countBuy'] ? totalData['countBuy'] + 1 : 1;
-            totalData['volumeBuy'] = totalData['volumeBuy'] ? totalData['volumeBuy'] + parseInt(item.action.twap.s) : parseInt(item.action.twap.s);
-          }
-
-          if (item.error === null) {
-            twapsTable.append(row);
-          }
-        });
+          if (!item.error) twapsTable.append(row);
+          const key = isSell ? 'Sell' : 'Buy';
+          acc[`count${key}`] = (acc[`count${key}`] || 0) + 1;
+          acc[`volume${key}`] = (acc[`volume${key}`] || 0) + parseInt(item.action.twap.s);
+          return acc;
+        }, {});
         dataExchange[TWAPsChartKey] = data;
         addTotalData(totalData);
       } else {
@@ -171,7 +111,7 @@ async function fetchTwaps() {
     },
     error: function (xhr, status, error) {
       console.error('Error fetching data:', error);
-      $('#TWAPs-table tbody').append('<tr><td colspan="2">Error fetching data</td></tr>');
+      $('#TWAPs-table tbody').append('<tr><td colspan="5">Error fetching data</td></tr>');
     }
   });
 }
@@ -222,8 +162,6 @@ function toggleTable() {
     $('.tab-content').removeClass('active');
     $(`#${targetTab}`).addClass('active');
     if (targetTab.includes('chart')) {
-      console.log(targetTab);
-
       buildChart(targetTab);
     }
   });
@@ -232,6 +170,7 @@ function toggleTable() {
 $(document).ready(function () {
   toggleTable();
   fetchTwaps();
-  setInterval(fetchTwaps, 60000); // Refresh data every 1 minutes
+  setInterval(fetchTwaps, 60000);
   sortTable();
 });
+
